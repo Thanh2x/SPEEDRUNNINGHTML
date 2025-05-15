d4b56c0f603a0be416207677c060dd5a97ea56c8
Thanh2x <tvhoang@ucsd.edu>
2025-05-14 23:19:53 -0700

    More learning

--- leaarning.js
+++ leaarning.js
@@ -109,7 +109,7 @@ switch (swti) {
 // Objects
 
 let myobject = {
-    'name': "bob",
+    name: "bob",
     'major': "CS",
     'age': 20,
     'favoriteFood': ['candy', 'pasta', 'chicken'],
@@ -125,7 +125,9 @@ for (let i = 0; i < myobject.favoriteFood.length; i++) {
 // Adding a new propertie to an object
 
 myobject.woof = "woofwoof";
+myobject['deeznuts'] = 'deeznuts';
 
+console.log(myobject['deeznuts']);
 console.log(myobject.woof);
 
 // Deleting a property from an object
@@ -135,3 +137,183 @@ delete myobject.woof;
 if (!myobject.hasOwnProperty("woof")) {
     console.log("Does not contain the woof");
 }
+
+
+// JSON.parse
+
+let index = 10;
+
+while (index > 0) {
+    console.log(index);
+    index--;
+}
+
+console.log(JSON.stringify(nums));
+
+console.log("here is a random number " + Math.floor(Math.random() * 100));
+
+let stringnum = "232";
+
+let numparse = parseInt(stringnum);
+
+console.log(typeof stringnum);
+console.log(typeof numparse);
+
+let binaryString = "1101";
+
+// For parse int, the second arugment deifnes the base, so this case it would be base 2, for binary
+let binaryNum = parseInt(binaryString, 2);
+
+// Conditional statemenst
+
+let numTest = -1;
+
+let numCon = numTest > 0 ? "positive" : numTest < 0 ? "negative" : "undefined";
+
+console.log(numCon);
+
+// A variable whose value can not change, it is read only, Arrays can be a const but you can still change it's values as it is a reference variable refering to an arra in memory address
+const CONSTVARIABLE = 10;
+
+// "use strict"; makes it that we don't make mistakes while coding
+
+const freezeObject = {
+    "value": 10,
+    "name": "thanh"
+};
+
+// freezing an object makes it so that the inner properties of the objects can't be change.
+Object.freeze(freezeObject);
+
+// Concatting arrays
+
+let arr1 = [1, 2, 3];
+let arr2 = [3, 4, 5];
+
+// Annoymous funcitons
+const concattingArras = (arr1, arr2) => arr1.concat(arr2);
+
+let myAnonymousFunction = () => {
+    console.log("HI");
+    return 1;
+};
+
+console.log(JSON.stringify(concattingArras(arr1, arr2)));
+
+var magic = function () {
+    return new Date();
+}
+
+var magic2 = (ab, vd) => {
+    return new Date();
+}
+
+var magic3 = () => new Date();
+
+let testData = magic();
+console.log(testData.toDateString());
+
+// Using window.prompt() for use input
+
+// let favoriteDog;
+// favoriteDog = window.prompt("What is your favorite dog breed?");
+
+// Type casting, 
+let x = "bob";
+let y = "bob";
+let z = "bob";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+// What is the difference between a number data type vs an integer
+
+let mynum = 1;
+console.log(typeof mynum);
+
+console.log(typeof x, x);
+
+// different ways of making functions
+
+let testfunct1 = function () {
+    return 1;
+}
+
+function testfunct2() {
+    return 2;
+}
+
+let testfunct3 = () => {
+    return 3;
+}
+
+let testfunct4 = () => 4;
+
+globalThis.g = 3;
+console.log("hi", globalThis.g);
+
+// test
+
+// function Pear() {
+//     this.age = 0;
+
+//     setInterval((() => { this.age++; }) => {
+//         console.log(${ this.age });
+//     }, 1000);
+// }
+
+class thanhpro {
+    constructor(name) {
+        this.name = name;
+        this.age = 10;
+    }
+
+    printName() {
+        console.log(this.name);
+        // setInterval(() => printAge(this.age), 1000);
+    }
+};
+
+function printAge(age) {
+    console.log("age is", age);
+}
+
+let me = new thanhpro("thah");
+me.printName();
+
+let collapse = (...agrs) => {
+    for (let i = 0; i < agrs.length; i++) {
+        console.log("This is ", agrs[i]);
+    }
+};
+
+collapse(1, 2, 3, 4, 5);
+
+// Default parameters
+
+let defaultParametersFunction = (a = 1, b = 2) => {
+    return a + b;
+};
+
+console.log(defaultParametersFunction("a"));
+
+// Copying arrays using the spread operator
+
+let spreadArr1 = [1, 2, 3, 4, 5];
+let spreadArr2 = [...spreadArr1];
+spreadArr1[0] = 11;
+
+console.log(spreadArr2);
+
+let someObject = {
+    name: "thanh",
+    bob: "pro",
+    age: 14
+};
+
+// Destructuring
+
+const { name: a, bob: b, age: c } = someObject;
+
+console.log(a, b, c);
